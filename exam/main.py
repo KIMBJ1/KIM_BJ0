e18862f3b12f35c13075e76e85e613a1bc4ecaf7
KIMBJ1 <57746855+KIMBJ1@users.noreply.github.com>
2020-06-24 15:58:23 +0900

    Add files via upload

---
+++ exam/main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, session
+import sys
+
+app = Flask(__name__)
+
+@app.route('/')
+def if_practice():
+    return render_template('asdf.html')
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
